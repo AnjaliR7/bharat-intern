2fe945b6ccecd2992c6ea1a57dda7a0d8e054bfe
AnjaliR7 <148202284+AnjaliR7@users.noreply.github.com>
2024-08-07 19:40:16 +0530

    Add files via upload

---
+++ task1/script.js
@@ -0,0 +1,4 @@
+// script.js
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Welcome to my portfolio! Here you can learn more about my projects and skills.');
+});
